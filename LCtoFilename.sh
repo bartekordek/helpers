a6962f1f38230480a109f19e58d1b9e9ea5e2f87
Bartłomiej Kordek <bartekordek@gmail.com>
2021-02-03 09:43:36 +0100

    Added creating files from template.

--- LCtoFilename.sh
+++ LCtoFilename.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xe
 
 # 1. To lower.
 RESULT=`echo $1 | tr '[:upper:]' '[:lower:]'`
@@ -8,9 +8,18 @@ RESULT=`echo $RESULT | sed 's/\.//'`
 
 # 3. Replace spaces with floors.
 RESULT=`echo $RESULT | sed 's/\ /_/g'`
+NEW_NAME="LC_$RESULT"
+echo "New name: $NEW_NAME"
+#echo "LC_$RESULT" > RESULT.txt
 
-echo "LC_$RESULT"
-echo "LC_$RESULT" > RESULT.txt
+HEADER_NAME="$NEW_NAME.hpp"
+SOURCE_NAME="$NEW_NAME.cpp"
 
-touch "LC_$RESULT.hpp"
-touch "LC_$RESULT.cpp"
+echo "Source name: $SOURCE_NAME"
+echo "Header name: $HEADER_NAME"
+
+cp TEMPLATE.hpp.tmpl $HEADER_NAME
+cp TEMPLATE.cpp.tmpl $SOURCE_NAME
+
+sed -i "s/TEMPLATE/$NEW_NAME/g" $HEADER_NAME
+sed -i "s/TEMPLATE/$NEW_NAME/g" $SOURCE_NAME
