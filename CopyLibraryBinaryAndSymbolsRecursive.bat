d15c44f5308354984e23f6afae478f69c4e81e49
Bartłomiej Kordek <bartekordek@gmail.com>
2021-02-17 19:14:55 +0100

    Fix for copying files.

--- CopyLibraryBinaryAndSymbolsRecursive.bat
+++ CopyLibraryBinaryAndSymbolsRecursive.bat
@@ -1,9 +1,9 @@
 Echo "Working directory: %cd%"
 
 For /R %1 %%G IN (*.dll) do (
-xcopy /Y /F %%G %2
+xcopy /Y /F /D %%G %2
 )
 
 For /R %1 %%G IN (*.pdb) do (
-xcopy /Y /F %%G %2
+xcopy /Y /F /D %%G %2
 )
